a56d03ae0a860c8f6acb86e2ff0ad48c4677158e
cain413 <140903105+cain413@users.noreply.github.com>
2024-03-07 10:30:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let monitorCount = function(rows, columns){
+  return (rows * columns)
+}
 
+const numOfMonitors = monitorCount(5,4);
+
+console.log(numOfMonitors);
+
+
+function monitorCount(rows, columns) {
+  const numOfMonitors = rows * columns;
+  return numOfMonitors;
+}
+
+console.log(monitorCount(5,4))
+
+
+
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+
+getFahrenheit(15); // Returns 59
